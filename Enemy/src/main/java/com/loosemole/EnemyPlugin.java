4adb0d201de293f6fc1544f752838147830d762d
looseMole <62012079+looseMole@users.noreply.github.com>
2024-02-28 12:18:55 +0100

    More discernable Enemy shape

--- Enemy/src/main/java/com/loosemole/EnemyPlugin.java
+++ Enemy/src/main/java/com/loosemole/EnemyPlugin.java
@@ -24,7 +24,7 @@ public class EnemyPlugin implements IGamePluginService {
 
     private Entity createEnemyShip(GameData gameData) {
         Entity enemyShip = new Enemy();
-        enemyShip.setPolygonCoordinates(-5, -5, 10, 0, -5, 2);
+        enemyShip.setPolygonCoordinates(12, -1, 8, -1, 8, -3, 6, -3, 6, -5, -2, -5, -2, -7, 0, -7, 0, -9, -10, -9, -10, -5, -8, -5, -8, -3, -6, -3, -6, -1, -10, -1, -10, 1, -6, 1, -6, 3, -8, 3, -8, 5, -10, 5, -10, 9, 0, 9, 0, 7, -2, 7, -2, 5, 2, 5, 2, 1, 4, 1, 4, -1, 2, -1, 2, -3, 4, -3, 4, -1, 6, -1, 6, 1, 4, 1, 4, 3, 2, 3, 2, 5, 6, 5, 6, 3, 8, 3, 8, 1, 12, 1);
         enemyShip.setX(Math.random()*gameData.getDisplayWidth());
         enemyShip.setY(0);
         return enemyShip;
