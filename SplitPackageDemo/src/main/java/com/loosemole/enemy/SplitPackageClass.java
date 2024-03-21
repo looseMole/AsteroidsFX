c363cbee870f26bfb7cc62b8a174b0212688d1e9
looseMole <62012079+looseMole@users.noreply.github.com>
2024-03-21 12:08:42 +0100

    Revert "Create +1 com.loosemole.enemy package, on purpose"
    This reverts commit 66f32359edf0de0427f40fbd462fae262c54fb56.

--- SplitPackageDemo/src/main/java/com/loosemole/enemy/SplitPackageClass.java
+++
@@ -1,7 +0,0 @@
-package com.loosemole.enemy;
-
-public class SplitPackageClass {
-    public static void splitPackageMethod()  {
-        System.out.println("This is a method from another class in a split package");
-    }
-}
--- SplitPackageDemo/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-import com.loosemole.enemy.SplitPackageClassPlugin;
-import dk.sdu.mmmi.cbse.common.services.IGamePluginService;
-
-module SplitPackageDemo {
-    requires Common;
-    provides IGamePluginService with SplitPackageClassPlugin;
-}
